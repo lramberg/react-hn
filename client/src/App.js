6591cbdfe64945752f85acb650c8010b65a1917f
Lyndsay Ramberg <lyndsayramberg@Lyndsays-MacBook-Pro.local>
2019-08-13 20:52:37 -0700

    set up Apollo Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import './App.css';
+
+// components
 import PostIndex from '../src/components/PostIndex';
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000'
+})
 
 class App extends Component {
     render() {
         return(
-        <div className="App">
-            <h1>Yo</h1>
-            <PostIndex />
-        </div>
+        <ApolloProvider client={ client }>
+            <div className="App">
+                <h1>Yo</h1>
+                <PostIndex />
+            </div>
+        </ApolloProvider>
         );
     }
 }
